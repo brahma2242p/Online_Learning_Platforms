d7d71930c6d3f5c30884dca9f2d350c7e37f91fe
brahma2242p <166218892+brahma2242p@users.noreply.github.com>
2025-03-05 15:28:31 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,8 @@
+const { default: mongoose } = require("mongoose");
 
+exports.db= mongoose.connect(process.env.DATABASE_URL).then(()=>{
+    console.log("database connected");
+}).catch((err)=>{
+    console.log("database error");
+    
+})
